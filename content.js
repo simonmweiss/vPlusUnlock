2c45e675c1f7b9d83e5956cf4a6784c26567c334
Simon <simonweiss@gmx.at>
2024-09-16 08:02:37 +0200

    change eventListener type

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+window.addEventListener('DOMContentLoaded', function () {
     var externalPostDataNode = document.getElementById("externalPostDataNode")
     if (externalPostDataNode) {
         unlockVPlus(externalPostDataNode);
